2955cad2d91e7056da86da4ef0a5683b8c339f72
aleruzza <alessandro.ruzza99@gmail.com>
2024-08-30 11:15:56 +0200

    fix

--- training/models.py
+++ training/models.py
@@ -237,6 +237,62 @@ class MultiPModel(keras.Model):
         }
         return {**base_config, **config}
 
+    #this is to fix 
+    @classmethod
+    def from_config(cls, config, custom_objects=None):
+        from keras.src.models.functional import Functional
+        config['res_blocks'] = custom_objects['n_res_blocks']
+        functional_config_keys = [
+            "name",
+            "layers",
+            "input_layers",
+            "output_layers",
+        ]
+        is_functional_config = all(
+            key in config for key in functional_config_keys
+        )
+        argspec = inspect.getfullargspec(cls.__init__)
+        functional_init_args = inspect.getfullargspec(Functional.__init__).args[
+            1:
+        ]
+        revivable_as_functional = (
+            cls in {Functional, Model}
+            or argspec.args[1:] == functional_init_args
+            or (argspec.varargs == "args" and argspec.varkw == "kwargs")
+        )
+        if is_functional_config and revivable_as_functional:
+            # Revive Functional model
+            # (but not Functional subclasses with a custom __init__)
+            from keras.src.models.functional import functional_from_config
+
+            return functional_from_config(
+                cls, config, custom_objects=custom_objects
+            )
+
+        # Either the model has a custom __init__, or the config
+        # does not contain all the information necessary to
+        # revive a Functional model. This happens when the user creates
+        # subclassed models where `get_config()` is returning
+        # insufficient information to be considered a Functional model.
+        # In this case, we fall back to provide all config into the
+        # constructor of the class.
+        try:
+            return cls(**config)
+        except TypeError as e:
+            raise TypeError(
+                "Unable to revive model from config. When overriding "
+                "the `get_config()` method, make sure that the "
+                "returned config contains all items used as arguments "
+                f"in the  constructor to {cls}, "
+                "which is the default behavior. "
+                "You can override this default behavior by defining a "
+                "`from_config(cls, config)` class method to specify "
+                "how to create an "
+                f"instance of {cls.__name__} from its config.\n\n"
+                f"Received config={config}\n\n"
+                f"Error encountered during deserialization: {e}"
+            )
+        
     def __init__(
         self,
         act="leaky_relu",
@@ -342,6 +398,8 @@ class MultiPModel(keras.Model):
 
     def get_smoothing_layer(self):
         return self.augm_layers[self.SMOOTHING_LAYER]
+    
+    
 
 
 def venus_multip(
