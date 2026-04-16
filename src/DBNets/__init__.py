113ab9525a2e6662c3087e3a16d7644ed49a2b7c
Alessandro Ruzza <35341919+aleruzza@users.noreply.github.com>
2026-04-16 15:55:55 +0200

    Add environment variable and warning filters
    Suppress TensorFlow logging and warnings on import.

--- src/DBNets/__init__.py
+++ src/DBNets/__init__.py
@@ -36,6 +36,10 @@ def _check_data_installed():
 
 # Run check at import
 import sys
+import os
+import warnings
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+warnings.filterwarnings("ignore")
 
 # If running "python -m dbnets.download_models", skip check
 if not ("dbnets.download_models" in sys.argv[0] or
